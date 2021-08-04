d592dab7a31f836ce7c1bc545624e41f74f07742
haseebjani <haseebansari472@gmail.com>
2021-08-04 22:04:28 +0500

    no change

---
+++ test.js
@@ -0,0 +1,24 @@
+var test = String(34);
+	console.log(test, typeof(test));
+
+var dat = new Date();
+	console.log(dat, (typeof dat));
+
+
+for(let i=100; i> 100; i++)
+	{
+		console.log(i);
+	}
+
+let a =34;
+	a +=1;
+console.log(a);
+
+
+let k=0;
+
+do{
+	
+	k++;
+}while(k<100);
+console.log(k);
